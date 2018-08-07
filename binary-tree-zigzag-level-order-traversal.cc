1efcf98fe596c66980734340c712af0b9f6d6d64
wisdom <wisdom.yinzh@gmail.com>
2018-08-07 23:45:54 +0800

    Aug 7

---
+++ binary-tree-zigzag-level-order-traversal.cc
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <sstream>
+#include <queue>
+#include <vector>
+#include <deque>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+
+        if (root == nullptr) {
+            return vector<vector<int>>();
+        }
+
+        deque<TreeNode *> current, next;
+        next.push_back(root);
+        int level = 0;
+
+        do {
+            current = next;
+            next.clear();
+
+            TreeNode *p;
+            while (!current.empty()) {
+                p = current.front();
+                current.pop_front();
+
+                if ((int)result.size() <= level) {
+                    result.push_back(vector<int>{ p->val });
+                } else {
+                    result[level].push_back(p->val);
+                }
+                if (p->left) {
+                    next.push_back(p->left);
+                }
+                if (p->right) {
+                    next.push_back(p->right);
+                }
+            }
+
+            if ((level & 1) == 1) {
+                std::reverse(result[level].begin(), result[level].end());
+            }
+
+            level++;
+        } while (!next.empty());
+
+
+        return result;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        
+        for (auto &row: Solution().zigzagLevelOrder(root)) {
+            cout << integerVectorToString(row) << endl;
+        }
+    }
+    return 0;
+}
---
+++ validate-binary-search-tree.cc
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <sstream>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool recursionIsValidBST(TreeNode *root, int &min, int &max) {
+        int lmin, lmax, rmin, rmax;
+        bool left, right;
+        bool result = true;
+
+        min = max = root->val;
+
+        if (root->left) {
+            left = recursionIsValidBST(root->left, lmin, lmax);
+            result &= (left && lmax < root->val);
+            min = lmin;
+        }
+        if (root->right) {
+            right = recursionIsValidBST(root->right, rmin, rmax);
+            result &= (right && rmin > root->val);
+            max = rmax;
+        }
+        return result;
+    }
+
+    bool isValidBST(TreeNode* root) {
+        if (root == nullptr) {
+            return true;
+        }
+        int lmin, lmax, rmin, rmax;
+        bool left, right;
+        bool result = true;
+        if (root->left) {
+            left = recursionIsValidBST(root->left, lmin, lmax);
+            result &= (left && lmax < root->val);
+        }
+        if (root->right) {
+            right = recursionIsValidBST(root->right, rmin, rmax);
+            result &= (right && rmin > root->val);
+        }
+        return result;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        
+        bool ret = Solution().isValidBST(root);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
