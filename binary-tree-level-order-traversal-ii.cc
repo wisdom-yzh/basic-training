279e8b899ea1c6cac8b93e194b68435ed4a4d5da
wisdom <wisdom.yinzh@gmail.com>
2018-08-08 23:21:40 +0800

    Aug 8

---
+++ binary-tree-level-order-traversal-ii.cc
@@ -0,0 +1,56 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        queue<std::pair<TreeNode *, int>> q;
+        vector<vector<int>> result;
+
+        if (root == nullptr) {
+            return vector<vector<int>>();
+        }
+
+        q.push(std::make_pair(root, 0));
+
+        while (!q.empty()) {
+            auto top = q.front();
+            if (top.second >= (int)result.size()) {
+                result.push_back(vector<int> { top.first->val });
+            } else {
+                result[top.second].push_back(top.first->val);
+            }
+            if (top.first->left != nullptr) {
+                q.push(std::make_pair(top.first->left, top.second + 1));
+            }
+            if (top.first->right != nullptr) {
+                q.push(std::make_pair(top.first->right, top.second + 1));
+            }
+            q.pop();
+        }
+
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
+int main() {
+    return 0;
+}
---
+++ convert-sorted-array-to-binary-search-tree.cc
@@ -0,0 +1,104 @@
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return nullptr;
+        }
+        if (nums.size() == 1) {
+            return new TreeNode(nums[0]);
+        }
+        int middle = nums.size() / 2;
+        vector<int> leftArr(nums.begin(), nums.begin() + middle);
+        vector<int> rightArr(nums.begin() + middle + 1, nums.end());
+        TreeNode *root = new TreeNode(nums[middle]);
+        root->left = sortedArrayToBST(leftArr);
+        root->right = sortedArrayToBST(rightArr);
+        return root;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string treeNodeToString(TreeNode* root) {
+    if (root == nullptr) {
+      return "[]";
+    }
+
+    string output = "";
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()) {
+        TreeNode* node = q.front();
+        q.pop();
+
+        if (node == nullptr) {
+          output += "null, ";
+          continue;
+        }
+
+        output += to_string(node->val) + ", ";
+        q.push(node->left);
+        q.push(node->right);
+    }
+    return "[" + output.substr(0, output.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        TreeNode* ret = Solution().sortedArrayToBST(nums);
+
+        string out = treeNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
