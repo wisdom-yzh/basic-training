e8616aa6a0c5737d4e5aef5da85c2cc9fef5149e
wisdom <wisdom.yinzh@gmail.com>
2018-09-06 23:45:19 +0800

    Sep 6

---
+++ count-complete-tree-nodes.cc
@@ -0,0 +1,108 @@
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <queue>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int height(TreeNode *root) {
+        return root == nullptr ? -1 : 1 + height(root->left);
+    }
+
+    int countNodes(TreeNode* root) {
+        int nodes = 0, h = height(root);
+        while (root != nullptr) {
+            if (height(root->right) == h - 1) {
+                nodes += 1 << h;
+                root = root->right;
+            } else {
+                nodes += 1 << (h - 1);
+                root = root->left;
+            }
+            h--;
+        }
+        return nodes;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        
+        int ret = Solution().countNodes(root);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
---
+++ maximal-square.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty()) {
+            return 0;
+        }
+        int m = matrix.size(), n = matrix[0].size(), sz = 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = matrix[0][j] - '0';
+            sz = max(sz, dp[0][j]);
+        }
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = matrix[i][0] - '0';
+            sz = max(sz, dp[i][0]);
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    sz = max(sz, dp[i][j]);
+                }
+            }
+        }
+        return sz * sz;
+    }
+};
+
+int main() {
+    vector<vector<char>> matrix {
+        { '1', '0', '1', '0', '0' },
+        { '1', '0', '1', '1', '1' },
+        { '1', '1', '1', '1', '1' },
+        { '1', '0', '0', '1', '0' },
+    };
+    return 0;
+}
