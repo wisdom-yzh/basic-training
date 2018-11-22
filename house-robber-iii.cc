8e80cb7849131f07c0bb28b7c2916a3ce4b9c9b9
wisdom <wisdom.yinzh@gmail.com>
2018-11-23 00:03:19 +0800

    Nov 22

---
+++ house-robber-iii.cc
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int max1 = rob(root->left) + rob(root->right);
+        int max2 = root->val;
+        if (root->left != nullptr) {
+            max2 += rob(root->left->left) + rob(root->left->right);
+        }
+        if (root->right != nullptr) {
+            max2 += rob(root->right->left) + rob(root->right->right);
+        }
+        return max(max1, max2);
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        
+        int ret = Solution().rob(root);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
---
+++ increasing-triplet-subsequence.cc
@@ -0,0 +1,68 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int c1 = numeric_limits<int>::max();
+        int c2 = numeric_limits<int>::max();
+        for (auto &num: nums) {
+            if (num <= c1) {
+                c1 = num;
+            } else if (num <= c2) {
+                c2 = num;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        bool ret = Solution().increasingTriplet(nums);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
