0c77a4bf24d50a5df03ccb4b29172f09e6b6e4db
wisdom <wisdom.yinzh@gmail.com>
2019-03-06 00:07:57 +0800

    Mar 06, 2019

---
+++ delete-node-in-a-bst.cc
@@ -0,0 +1,182 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == nullptr) {
+            return root;
+        }
+
+        enum { Left = 0, Right } position;
+        TreeNode *parent = nullptr;
+        auto *p = root;
+
+        while (p->val != key) {
+            parent = p;
+            if (p->val > key) {
+                position = Left;
+                p = p->left;
+            } else {
+                position = Right;
+                p = p->right;
+            }
+            if (p == nullptr) {
+                return root;
+            }
+        }
+
+        if (!p->left && !p->right) {
+            delete p;
+            if (parent == nullptr) {
+                root = p = nullptr;
+            } else if (position == Left) {
+                parent->left = nullptr;
+            } else {
+                parent->right = nullptr;
+            }
+        } else if (p->left && !p->right) {
+            auto *tmp = p;
+            if (parent == nullptr) {
+                root = p = p->left;
+            } else if (position == Left) {
+                parent->left = p->left;
+            } else {
+                parent->right = p->left;
+            }
+            delete tmp;
+        } else {
+            int value = this->findMinValueLargerThanCurrent(p);
+            this->deleteNode(p, value);
+            p->val = value;
+        }
+        return root;
+    }
+
+    int findMinValueLargerThanCurrent(TreeNode *root) {
+        auto *p = root->right;
+        int value = p->val;
+
+        while (p->left != nullptr) {
+            p = p->left;
+            value = p->val;
+        }
+
+        return value;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string treeNodeToString(TreeNode* root) {
+    if (root == nullptr) {
+      return "[]";
+    }
+
+    string output = "";
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()) {
+        TreeNode* node = q.front();
+        q.pop();
+
+        if (node == nullptr) {
+          output += "null, ";
+          continue;
+        }
+
+        output += to_string(node->val) + ", ";
+        q.push(node->left);
+        q.push(node->right);
+    }
+    return "[" + output.substr(0, output.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        getline(cin, line);
+        int key = stringToInteger(line);
+
+        TreeNode* ret = Solution().deleteNode(root, key);
+
+        string out = treeNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
---
+++ serialize-and-deserialize-bst.cc
@@ -0,0 +1,61 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Codec {
+public:
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string order;
+        inorderDFS(root, order);
+        return order;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        int pos = 0;
+        return reconstruct(data, pos, INT32_MIN, INT32_MAX);
+    }
+
+    inline void inorderDFS(TreeNode* root, string& order) {
+        if (!root) return;
+        char buf[4];
+        memcpy(buf, &(root->val), sizeof(int));
+
+        for (int i = 0; i < 4; i++) {
+            order.push_back(buf[i]);
+        }
+        inorderDFS(root->left, order);
+        inorderDFS(root->right, order);
+    }
+
+    inline TreeNode* reconstruct(const string& buffer, int& pos, int minValue, int maxValue) {
+        if (pos >= buffer.size()) return NULL;
+
+        int value;
+        memcpy(&value, &buffer[pos], sizeof(int));
+        if (value < minValue || value > maxValue) return NULL;
+
+        TreeNode* node = new TreeNode(value);
+        pos += sizeof(int);
+        node->left = reconstruct(buffer, pos, minValue, value);
+        node->right = reconstruct(buffer, pos, value, maxValue);
+        return node;
+    }
+};
+
+int main() {
+    return 0;
+}
