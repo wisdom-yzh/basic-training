6506fed03f57783e3103ed8a77190701cf6ea531
wisdom <wisdom.yinzh@gmail.com>
2019-06-15 01:36:46 +0800

    Jun 15, 2019

---
+++ coin-change-2.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+
+        for (const auto &coin: coins) {
+            for (int i = 1; i <= amount; i++) {
+                if (i >= coin) {
+                    dp[i] += dp[i - coin];
+                }
+            }
+        }
+        return dp[amount];
+    }
+};
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        int amount = stringToInteger(line);
+        getline(cin, line);
+        vector<int> coins = stringToIntegerVector(line);
+
+        int ret = Solution().change(amount, coins);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
