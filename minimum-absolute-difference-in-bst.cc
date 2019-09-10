023f0192a7d2389c454c7abaaaa7de1eb6c1fbc4
wisdom <wisdom.yinzh@gmail.com>
2019-09-11 00:37:08 +0800

    Sep 11, 2019

---
+++ minimum-absolute-difference-in-bst.cc
@@ -0,0 +1,115 @@
+#include <queue>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <limits>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    void inorderTraverse(TreeNode *root, int &val, int &minDiff) {
+        if (root->left) {
+            inorderTraverse(root->left, val, minDiff);
+        }
+
+        if (val != -1) {
+            minDiff = std::min(root->val - val, minDiff);
+        }
+
+        val = root->val;
+
+        if (root->right) {
+            inorderTraverse(root->right, val, minDiff);
+        }
+    }
+
+    int getMinimumDifference(TreeNode* root) {
+        int minDiff = std::numeric_limits<int>::max();
+        int val = -1;
+        this->inorderTraverse(root, val, minDiff);
+        return minDiff;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+
+        int ret = Solution().getMinimumDifference(root);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
