4dd4d5b996c7bbd11ab2484b473674843f36372f
wisdom <wisdom.yinzh@gmail.com>
2020-07-19 10:54:54 +0800

    Jul 19, 2020

---
+++ binary-subarrays-with-sum.cc
@@ -0,0 +1,71 @@
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& A, int S) {
+        int result = 0;
+        std::map<int, int> dict;
+        dict[0] = 1;
+
+        int sum = 0;
+        for (int i = 0; i < A.size(); i++) {
+            sum += A[i];
+            result += dict[sum - S];
+            dict[sum]++;
+        }
+
+        return result;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> A = stringToIntegerVector(line);
+        getline(cin, line);
+        int S = stringToInteger(line);
+
+        int ret = Solution().numSubarraysWithSum(A, S);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
---
+++ minimum-falling-path-sum.cc
@@ -0,0 +1,37 @@
+#include <vector>
+#include <iostream>
+#include <limits>
+using namespace std;
+
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& A) {
+        const int rows = A.size();
+        const int cols = A[0].size();
+        vector<vector<int>> dp(A.size(), vector<int>(A[0].size(), 0));
+
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = 0; j < cols; j++) {
+                if (i == rows - 1) {
+                    dp[i][j] = A[i][j];
+                } else {
+                    dp[i][j] = A[i][j] + dp[i + 1][j];
+                    if (j != 0) {
+                        dp[i][j] = std::min(dp[i][j], A[i][j] + dp[i + 1][j - 1]);
+                    }
+                    if (j != cols - 1) {
+                        dp[i][j] = std::min(dp[i][j], A[i][j] + dp[i + 1][j + 1]);
+                    }
+                }
+            }
+        }
+
+        return *std::min_element(dp[0].begin(), dp[0].end());
+    }
+};
+
+int main() {
+    vector<vector<int>> testCase { {1,2,3},{4,5,6},{7,8,9} };
+    cout << Solution().minFallingPathSum(testCase);
+    return 0;
+}
